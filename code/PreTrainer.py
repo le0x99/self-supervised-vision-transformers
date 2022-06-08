56ab1dcdb9e37cdb994533c552ef8c6164654af7
Leonard D. Vorbeck <36996220+le0x99@users.noreply.github.com>
2022-06-08 15:07:36 +0200

    Update PreTrainer.py

--- code/PreTrainer.py
+++ code/PreTrainer.py
@@ -13,8 +13,10 @@ import subprocess
 from torch.utils.tensorboard import SummaryWriter
 from torch.nn.modules.loss import CrossEntropyLoss, NLLLoss
 from torch.nn.modules.loss import SmoothL1Loss, MSELoss
-from PatchLocalizer import ContextLearner, MaskedContextLearner, MaskedPatchRegressor
+from PatchLocalizer import MaskedContextLearner
 from copy import deepcopy as copy
+from utils.base_models import PatchDropout, CNN
+
 
 torch.manual_seed(42)
 
@@ -27,7 +29,7 @@ pt_train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
                                        transforms.Normalize((0.5,), (0.5,))
                                        ])
 ft_train_transforms = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                       transforms.RandomResizedCrop(size=96, scale=(0.6, 1.0)),
+                                       transforms.RandomResizedCrop(size=96, scale=(0.8, 1.0)),
                                        transforms.RandomGrayscale(p=0.2),
                                        torchvision.transforms.RandomApply([transforms.GaussianBlur(kernel_size=9, sigma=(0.1, 0.5))], 0.5),
                                        transforms.ToTensor(),
@@ -44,7 +46,7 @@ ft_test_transforms = transforms.Compose([
 class Trainer(object):
     def __init__(self, s, verbose=False):
         self.pt_settings = s
-        self.name = f'[{s["METHOD"]},{s["G"]},{s["CROP"]}]-[{s["LEARNING_RATE"]},{s["BATCH_SIZE"]}]-[{s["N_BLOCKS"]},{s["HEADS"]},{s["HIDDEN_MULT"]},{1 if s["PROJECT_PATCH_EMB"] else 0},{1 if s["NN_HEAD"] else 0}, {s["DROPOUT"]}]'
+        self.name = f'[{s["PDO"]},{s["G"]},{s["CROP"]}]-[{s["LEARNING_RATE"]},{s["BATCH_SIZE"]}]-[{s["N_BLOCKS"]},{s["HEADS"]},{s["HIDDEN_MULT"]},{1 if s["NN_HEAD"] else 0}, {s["DROPOUT"]}]'
         self.name += "-[CNN]" if s["ENCODER"] == "CNN" else ''
         self.name += f'-[{s["COMMENT"]}]' if len(s["COMMENT"]) else ''
         self.dir = f'./{self.name}'
@@ -77,15 +79,6 @@ class Trainer(object):
                     }
         torch.save(state, self.dir + "/last_state")
          
-    def imshow(self, trainloader, n=6):
-        imgs = torch.stack([trainloader.dataset[i][0] for i in range(n)])
-        img_grid = torchvision.utils.make_grid(imgs, nrow=n, normalize=True, pad_value=0.9)
-        img_grid = img_grid.permute(1, 2, 0)
-        plt.figure(figsize=(10,5))
-        plt.imshow(img_grid)
-        plt.axis('off')
-        plt.show()
-        plt.close()
         
     def load_pt_data(self):
         s = self.pt_settings
@@ -113,13 +106,8 @@ class Trainer(object):
             subprocess.Popen(f'tensorboard --logdir=runs --port={self.port}', shell=True)
             
         step = 0
-        
-        if s["METHOD"] == "PLT":
-            model = ContextLearner(self.pt_settings)
-            train = self.train_plt
-        elif s["METHOD"] == "PLT_MASKED":
-            model = MaskedContextLearner(self.pt_settings)
-            train = self.train_plt_m
+        start_epoch = 1
+        model = MaskedContextLearner(self.pt_settings)
         optimizer = optim.Adam(model.parameters(), lr=s["LEARNING_RATE"], weight_decay=s["WEIGHT_DECAY"])
         warmup_epochs = int(s["NUM_EPOCHS"] * 0.05)
         warmup = optim.lr_scheduler.LinearLR(optimizer, start_factor=0.5 , total_iters=warmup_epochs)
@@ -127,53 +115,30 @@ class Trainer(object):
         scheduler = optim.lr_scheduler.ChainedScheduler([warmup, cosan])
         if continue_training:
             state = torch.load(self.dir + "/last_state")
+            state["scheduler"]["_schedulers"][-1]["T_max"] = s["NUM_EPOCHS"]
             model.load_state_dict(state['state_dict'])
             optimizer.load_state_dict(state['optimizer'])
             scheduler.load_state_dict(state["scheduler"])
             step = state["step"]
+            start_epoch = state["epoch"]
+            del state
+            torch.cuda.empty_cache()
         print(" - - - - Starting Training - - - - ")
-        train(model, optimizer, scheduler, trainloader, step)
+        print(f'PID : {os.getpid()}')
+        self.train_(model, optimizer, scheduler, trainloader, step, start_epoch)
             
     @torch.no_grad()
     def top_acc(self, Y, target, k):
         probs, classes = Y.topk(k)
         return torch.where(classes == target.unsqueeze(dim=1).expand_as(classes), 1., 0.).mean(0).cumsum(0)
-    
-    def train_plt(self, model, optimizer, scheduler, trainloader, step):
-        s = self.pt_settings
-        criterion = CrossEntropyLoss()
-        scaler = torch.cuda.amp.GradScaler()
-        for epoch in range(1, s["NUM_EPOCHS"]+1):
-            for data in trainloader:
-                X = data[0].to(s["DEVICE"])
-                target = torch.arange(model.T, device=s["DEVICE"]).repeat(s["BATCH_SIZE"])
-                optimizer.zero_grad()
-                
-                with torch.cuda.amp.autocast():
-                    logits = model(X)
-                    loss = criterion(logits, target)
-                
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-                
-                accs = self.top_acc(logits, target, 5)
-                self.logger.add_scalar("Loss/train", loss, step)
-                self.logger.add_scalar("Accuracy/train", accs[0], step)
-                self.logger.add_scalar("Accuracy/train3", accs[2], step)
-                self.logger.add_scalar("Accuracy/train5", accs[4], step)
-                step += 1
-                
-            scheduler.step()
-            self.save_state(model, optimizer, scheduler, step, epoch)
-            lr = scheduler.get_last_lr()[0]
-            self.logger.add_scalar("Loss/lr", lr, step)
+
             
-    def train_plt_m(self, model, optimizer, scheduler, trainloader, step=0):
+    def train_(self, model, optimizer, scheduler, trainloader, step=0, start_epoch=1):
         s = self.pt_settings
         criterion = CrossEntropyLoss()
         scaler = torch.cuda.amp.GradScaler()
-        for epoch in range(1, s["NUM_EPOCHS"]+1):
+        torch.cuda.empty_cache()
+        for epoch in range(start_epoch, s["NUM_EPOCHS"]+1):
             for data in trainloader:
                 X = data[0].to(s["DEVICE"])
                 target = torch.arange(model.T, device=s["DEVICE"]).repeat(s["BATCH_SIZE"])
@@ -199,18 +164,23 @@ class Trainer(object):
             lr = scheduler.get_last_lr()[0]
             self.logger.add_scalar("Loss/lr", lr, step)
                 
-    def train_mpr(self, model, optimizer, scheduler, trainloader, step=0):
-        pass
 
 
 class Tuner(object):
     def __init__(self, s):
         self.model_name = s["MODEL_NAME"]
-        self.name = f'({1 if s["HEAD_ONLY"] else 0},{1 if s["MLP_HEAD"] else 0},{1 if s["FREEZE_ENCODER"] else 0},{s["BATCH_SIZE"]},{s["LEARNING_RATE"]},{s["WEIGHT_DECAY"]},{s["DROPOUT"]}) - '+ self.model_name
-        self.name += f'-[{s["COMMENT"]}]' if len(s["COMMENT"]) else ''
-        self.settings = s
+        if self.model_name not in ["S-CNN", "S-ResNet"]:
+            self.name = f'({1 if s["HEAD_ONLY"] else 0},{1 if s["MLP_HEAD"] else 0},{1 if s["FREEZE_ENCODER"] else 0},{s["BATCH_SIZE"]},{s["LEARNING_RATE"]},{s["WEIGHT_DECAY"]},{s["DROPOUT"]},{s["PDO"]}) - '+ self.model_name
+            self.name += f'-[{s["COMMENT"]}]' if len(s["COMMENT"]) else ''
+            self.name += "-[BENCHMARK]-" if s["BENCHMARK"] else ''
+            self.dir = f'./{self.model_name}'
+            self.baseline = False
+        else:
+            self.name = self.model_name
+            self.name += f'-[{s["COMMENT"]}]' if len(s["COMMENT"]) else ''
+            self.baseline = True
         self.port = 6008
-        self.dir = f'./{self.model_name}'
+        self.settings = s
         
     def save_state(self, model, optimizer, step, epoch):
         self.logger.flush()
@@ -223,15 +193,6 @@ class Tuner(object):
                     }
         torch.save(state, self.dir + "/last_state")
          
-    def imshow(self, trainloader, n=6):
-        imgs = torch.stack([trainloader.dataset[i][0] for i in range(n)])
-        img_grid = torchvision.utils.make_grid(imgs, nrow=n, normalize=True, pad_value=0.9)
-        img_grid = img_grid.permute(1, 2, 0)
-        plt.figure(figsize=(10,5))
-        plt.imshow(img_grid)
-        plt.axis('off')
-        plt.show()
-        plt.close()
     
     def load_test_split(self):
         s = self.settings
@@ -241,15 +202,15 @@ class Tuner(object):
                                               transform=ft_train_transforms)
         train = self.reduce_training_size(train, npc) if npc < 500 else train
         trainloader = torch.utils.data.DataLoader(train, batch_size=s["BATCH_SIZE"],
-                                                              shuffle=True, num_workers=2,
-                                                              drop_last=False, pin_memory=True)
+                                                              shuffle=True, num_workers=4,
+                                                              drop_last=False, pin_memory=False)
 
         test = torchvision.datasets.STL10("../STL10/train/",
                                              split="test",
                                               transform=ft_test_transforms)
         testloader = torch.utils.data.DataLoader(test, batch_size=s["BATCH_SIZE"],
-                                                  shuffle=True, num_workers=2,
-                                                  drop_last=True, pin_memory=True)
+                                                  shuffle=True, num_workers=4,
+                                                  drop_last=True, pin_memory=False)
         return trainloader, testloader
             
         
@@ -270,32 +231,26 @@ class Tuner(object):
         self.logger = SummaryWriter(log_dir = "./ft_runs/"+self.name, comment=s["COMMENT"])
         if self.settings["SPAWN_TENSORBOARD"]:
             subprocess.Popen(f'tensorboard --logdir=ft_runs --port={self.port}', shell=True)
-        state = torch.load(self.dir + "/last_state")
-        pts = copy(state["pt_settings"])
-        pts["DROPOUT"] = s["DROPOUT"]
-        if pts["METHOD"] == "PLT":
-            model = ContextLearner(pts)         
-            model.load_state_dict(state['state_dict'])
-            model.ft_mode(s)
-            print(" - - - - Starting FineTune Training - - - - ")
-            self.fine_tune(model, trainloader, testloader)
-            
-        elif pts["METHOD"] == "PLT_MASKED":
+        if not self.baseline:
+            state = torch.load(self.dir + "/last_state")
+            pts = copy(state["pt_settings"])
+            pts["DROPOUT"] = s["DROPOUT"]
             model = MaskedContextLearner(pts)
-            model.load_state_dict(state['state_dict'])
-            model.ft_mode(s)
-            print(" - - - - Starting FineTune Training - - - - ")
-            self.fine_tune(model, trainloader, testloader)
-            
-        elif pts["METHOD"] == "MPR":
-            model = MaskedPatchRegressor(pts)
-            model.load_state_dict(state['state_dict'])
-            model.ft_mode(s)
-            print(" - - - - Starting FineTune Training - - - - ")
-            self.fine_tune(model, trainloader, testloader)
-            
+            if not self.settings["BENCHMARK"]:
+                print("Loading Model...")
+                model.load_state_dict(state['state_dict'])
+                model.ft_mode(s)
+            del state
+            torch.cuda.empty_cache()
         else:
-            raise NotImplemented 
+            if "S-CNN" in self.name:
+                model = CNN().to(s["DEVICE"])
+                model.ft_forward = model.forward
+            elif "S-ResNet" in self.name:
+                model = torchvision.models.resnet18(pretrained=False, num_classes=10).to(s["DEVICE"])
+                model.ft_forward = model.forward
+        print(" - - - - Starting FineTune Training - - - - ")
+        self.fine_tune(model, trainloader, testloader)
 
             
     @torch.no_grad()
@@ -306,6 +261,7 @@ class Tuner(object):
     @torch.no_grad()
     def test_model(self, model, criterion, testloader, epoch):
         s = self.settings
+        name = "test" if s["NPC"] == 500 else f'test-{s["NPC"]}'
         accs = []
         losses = []
         for data in testloader:
@@ -320,10 +276,10 @@ class Tuner(object):
 
         accs = torch.cat(accs, -1).mean(-1)
         losses = np.mean(losses)
-        self.logger.add_scalar("test/Acc@1", accs[0], epoch)
-        self.logger.add_scalar("test/Acc@3", accs[2], epoch)
-        self.logger.add_scalar("test/Acc@5", accs[4], epoch)
-        self.logger.add_scalar("test/NLL", losses, epoch)
+        self.logger.add_scalar(f'{name}/Acc@1', accs[0], epoch)
+        self.logger.add_scalar(f'{name}/Acc@3', accs[2], epoch)
+        self.logger.add_scalar(f'{name}/Acc@5', accs[4], epoch)
+        self.logger.add_scalar(f'{name}/NLL', losses, epoch)
         
     def fine_tune(self, model, trainloader, testloader):
         s = self.settings
@@ -336,7 +292,8 @@ class Tuner(object):
                               weight_decay=s["WEIGHT_DECAY"])
         warmup_epochs = int(s["NUM_EPOCHS"] * 0.1)
         warmup = optim.lr_scheduler.LinearLR(optimizer, start_factor=0.5 , total_iters=warmup_epochs)
-        cosan = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=s["NUM_EPOCHS"], eta_min= 0.1 * s["LEARNING_RATE"])
+        cosan = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=s["NUM_EPOCHS"],
+                                                     eta_min= 0.1 * s["LEARNING_RATE"])
         scheduler = optim.lr_scheduler.ChainedScheduler([warmup, cosan])
         for epoch in range(1, s["NUM_EPOCHS"]+1):
             for data in trainloader:
@@ -363,9 +320,13 @@ class Tuner(object):
             lr = scheduler.get_last_lr()[0]
             self.logger.add_scalar("train/lr", lr, step)
             
-            if step%s["EVAL_EPS"]==0:
+            if epoch%s["EVAL_EPS"]==0:
                 model.eval()
+                if not self.baseline:
+                    model.pdo = PatchDropout(0.)
                 self.test_model(model, criterion, testloader, epoch)
+                if not self.baseline:
+                    model.pdo = PatchDropout(self.settings["PDO"])
                 model.train()
 
 
